b12c41b28fbdb141acd80bdbd524a1dd647f1ced
Михаил <109952823+ScherbakovM@users.noreply.github.com>
2022-10-30 00:13:16 +0300

    noCalc

--- sсript.js
+++ sсript.js
@@ -1,23 +1,24 @@
 
-//Кнопкb вниз 
+//Кнопки вниз 
 const btn = document.getElementById('scrollB2');
 const btnSecond = document.getElementById('scrollB3');
+const btnThree = document.getElementById('scrollB4')
 
-console.log(btnSecond);
 
 //Кнопки вверх
 const btnTopOne = document.getElementById('top_One');
 const btnTopTwo = document.getElementById('top_Two');
+const btnTopThree = document.getElementById('top_Three');
 
 //Блоки
 const block1 = document.getElementById('calcForm');
 const block2 = document.getElementById('descriptionNorms');
 const block3 = document.getElementById('descriptionCeiling');
-console.log(block3);
+const block4 = document.getElementById('calculation');
 
+// Действия по клику.
 
-// Команды
-
+//Скроллы вниз
 
 btn.addEventListener('click', (scroll1) =>  { 
     block2.scrollIntoView({block: "center", behavior: "smooth"}); 
@@ -27,6 +28,12 @@ btnSecond.addEventListener('click', (scroll2) => {
     block3.scrollIntoView({block: "center", behavior: "smooth"});
 } );
 
+btnThree.addEventListener('click', (scroll5) =>  { 
+    block4.scrollIntoView({block: "center", behavior: "smooth"}); 
+});
+
+// Скрорхллы вверх
+
 btnTopOne.addEventListener('click', (scroll3) =>  { 
     block1.scrollIntoView({block: "center", behavior: "smooth"}); 
 });
@@ -34,3 +41,15 @@ btnTopOne.addEventListener('click', (scroll3) =>  {
 btnTopTwo.addEventListener('click', (scroll4) =>  { 
     block2.scrollIntoView({block: "center", behavior: "smooth"}); 
 });
+
+btnTopThree.addEventListener('click', (scroll5) =>  { 
+    block3.scrollIntoView({block: "center", behavior: "smooth"}); 
+});
+
+
+//Блок для расчета 
+
+// Инпуты 
+
+const blockE = document.getElementById('blockE');
+console.log(blockE);
